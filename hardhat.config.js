61e688b706f6c97017641d7808ab1450d02abfe4
FILIPNFT <99673408+FILIPNFT@users.noreply.github.com>
2024-07-06 13:04:46 +0200

    hardhat conf

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: "",
+      accounts: ['']
+    }
+  }
+};
